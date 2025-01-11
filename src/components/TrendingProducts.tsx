39d1b0c9319eaa700b67e3253b62e9fa4cd59da4
Suyash <suyashrusia2005@gmail.com>
2025-01-11 21:08:58 +0530

    Product card done

--- src/components/TrendingProducts.tsx
+++ src/components/TrendingProducts.tsx
@@ -3,49 +3,49 @@ import ProductCard from "./ProductCard";
 const products = [
     {
         id: 1,
-        img: '../../public/books/image1.webp',
+        img: '/books/image1.webp',
         genre: 'Fiction',
         title: 'The Great Gatsby',
         price: 19.99
     },
     {
         id: 2,
-        img: '../../public/books/image2.webp',
+        img: '/books/image2.webp',
         genre: 'Fiction',
         title: 'To Kill a Mockingbird',
         price: 29.99
     },
     {
         id: 3,
-        img: '../../public/books/image3.webp',
+        img: '/books/image3.webp',
         genre: 'Science Fiction',
         title: '1984',
         price: 15.99
     },
     {
         id: 4,
-        img: '../../public/books/image4.webp',
+        img: '/books/image4.webp',
         genre: 'Fiction',
         title: 'Pride and Prejudice',
         price: 25.99
     },
     {
         id: 5,
-        img: '../../public/books/image5.webp',
+        img: '/books/image5.webp',
         genre: 'Fiction',
         title: 'The Catcher in the Rye',
         price: 9.99
     },
     {
         id: 6,
-        img: '../../public/books/image6.webp',
+        img: '/books/image6.webp',
         genre: 'Fantasy',
         title: 'The Hobbit',
         price: 12.99
     },
     {
         id: 7,
-        img: '../../public/books/image7.webp',
+        img: '/books/image7.webp',
         genre: 'Fiction',
         title: 'Moby Dick',
         price: 22.99
@@ -53,7 +53,7 @@ const products = [
     ,
     {
         id: 8,
-        img: '../../public/books/image8.webp',
+        img: '/books/image8.webp',
         genre: 'Mystery',
         title: 'The Da Vinci Code',
         price: 18.99
