10a299075e77390d646d2e3a7fc2ab5b9647e249
Suyash <suyashrusia2005@gmail.com>
2025-01-11 21:10:15 +0530

    banner done

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,8 +3,8 @@ export default function Banner() {
         <div className="container mt-32">
             <div className="grid lg:grid-cols-2 gap-8 pr-[15px]">
 
-                <div className="h-[200px] md:h-[260px] bg-[url(../../public/banner1.jpg)] bg-cover bg-center rounded-xl p-8 md:p-16"></div>
-                <div className="h-[200px] md:h-[260px] bg-[url(../../public/banner2.jpg)] bg-cover bg-center rounded-xl p-8 md:p-16"></div>
+                <div className="h-[200px] md:h-[260px] bg-[url(/banner1.jpg)] bg-cover bg-center rounded-xl p-8 md:p-16"></div>
+                <div className="h-[200px] md:h-[260px] bg-[url(/banner2.jpg)] bg-cover bg-center rounded-xl p-8 md:p-16"></div>
             </div>
         </div>
     )
